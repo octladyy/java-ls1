0385068e70337c1096a02fa97643a5b9d3a3fd5a
aichurok <aichurok0798@gmail.com>
2024-12-11 17:57:32 +0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
         long longValue = 1434567L;
         float floatValue = 143.48f;
         double doubleValue = -2.3243;
-        System.out.println("Значение переменной numberInt с типом int равно " + numberInt );
+        System.out.println("Значение переменной numberInt с типом int равно " + numberInt);
         System.out.println("Значение переменной smallNumber с типом byte равно " + smallNumber);
         System.out.println("Значение переменной shortValue с типом short равно " + shortValue);
         System.out.println("Значение переменной longValue с типом long равно " + longValue);
@@ -34,21 +34,21 @@ public class Main {
 • 27897
 • 67 */
 
-        doubleValue = 27.12;
-        longValue = 987678965549L;
-        floatValue = 2.786F;
-        numberInt = 569;
-        shortValue = -159;
-        numberInt = 27897;
-        shortValue = 67;
+        double doubleSecondValue = 27.12;
+        long longSecondValue = 987678965549L;
+        float floatSecondValue = 2.786F;
+        int numberSecondInt = 569;
+        short shortSecondValue = -159;
+        int numberThirdInt = 27897;
+        short shortThirdValue = 67;
 
-        System.out.println(doubleValue);
-        System.out.println(longValue);
-        System.out.println(floatValue);
-        System.out.println(numberInt);
-        System.out.println(shortValue);
-        System.out.println(numberInt);
-        System.out.println(shortValue);
+        System.out.println(doubleSecondValue);
+        System.out.println(longSecondValue);
+        System.out.println(floatSecondValue);
+        System.out.println(numberSecondInt);
+        System.out.println(shortSecondValue);
+        System.out.println(numberThirdInt);
+        System.out.println(shortThirdValue);
 
         /*Задача 3
         Производительность машины для изготовления бутылок — 16 бутылок за 2 минуты. Какая производительность машины будет:
@@ -58,25 +58,29 @@ public class Main {
 • за 1 месяц?*/
 
         int bottlesInTwoMinutes = 16;
-        int bottlesInOneMinutes = bottlesInTwoMinutes / 2;
+        int bottlesMinutes = bottlesInTwoMinutes / 2;
+        int bottlesInTwentyMinute = bottlesMinutes * 8;
+        int bottlesOneDay = bottlesMinutes * 60 * 24;
+        int bottlesThreeDays = bottlesMinutes * 60 * 24 * 3;
+        int bottlesOneMonth = bottlesMinutes * 60 * 24 * 30;
 
-        System.out.println("за 20 минут: " + bottlesInOneMinutes * 8 + " бутылок");
-        System.out.println("в сутки: " + bottlesInOneMinutes * 60 * 24 + " бутылок");
-        System.out.println("за 3 дня: " + bottlesInOneMinutes * 60 * 24 * 3 + " бутылок");
-        System.out.println("за 1 месяц: " +bottlesInOneMinutes * 60 * 24 * 30 +" бутылок");
+        System.out.println("за 20 минут: " + bottlesInTwentyMinute * 8 + " бутылок");
+        System.out.println("в сутки: " + bottlesOneDay * 60 * 24 + " бутылок");
+        System.out.println("за 3 дня: " + bottlesThreeDays * 60 * 24 * 3 + " бутылок");
+        System.out.println("за 1 месяц: " + bottlesOneMonth * 60 * 24 * 30 + " бутылок");
 
         /*Задача 4: Перевод минут в часы
         Описание:
         Напишите программу, которая преобразует количество минут в часы и минуты.*/
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количсество минут: ");
+        System.out.print("Введите количество минут: ");
         int minutes = scanner.nextInt();
 
         int hours = minutes / 60;
         int minute = minutes % 60;
 
-        System.out.println(hours + " часа и " + minute + " минут" );
+        System.out.println(hours + " часа и " + minute + " минут");
 
 
        /* Задача 5: Определение возраста
@@ -86,18 +90,55 @@ public class Main {
 • Если возраст больше 60, выводится "Вы в зрелом возрасте"*/
 
         Scanner scannerAge = new Scanner(System.in);
-        System.out.println("Введите ваш возраст: ");
+        System.out.print("Введите ваш возраст: ");
         int age = scannerAge.nextInt();
 
         if (age < 18) {
             System.out.println("Вы еще молоды");
-        }
-        else if (age >= 18 && age <= 60) {
+        } else if (age >= 18 && age <= 60) {
             System.out.println("Вы в самом расцвете сил");
-        }
-        else {
+        } else {
             System.out.println("Вы в зрелом возрасте");
         }
-        scanner.close();
+
+        /*Задача 6:*Оценка по баллам
+        Описание: Напишите программу, которая присваивает оценку на основе баллов, заданных в переменной:
+• 90 и выше — "Отлично".
+• От 70 до 89 — "Хорошо".
+• От 50 до 69 — "Удовлетворительно".
+• Менее 50 — "Неудовлетворительно"*/
+
+        System.out.print("Введите вашу оценку: ");
+        int grade = scanner.nextInt();
+
+        if (grade >= 90) {
+            System.out.println("Отлично");
+        } else if (grade >= 70) {
+            System.out.println("Хорошо");
+        } else if (grade >= 50) {
+            System.out.println("Удовлетворительно");
+        } else {
+            System.out.println("Неудовлетворительно");
+        }
+
+       /* Задача 7: Определение наибольшего числа
+        Описание: Напишите программу, которая находит большее из двух чисел, заданных в переменных.*/
+
+        System.out.print("Введите первое число: ");
+        int numberOne = scanner.nextInt();
+
+        System.out.print("Введите второе число:");
+        int numberTwo = scanner.nextInt();
+
+        if (numberOne > numberTwo) {
+            System.out.print("Большее число: " + numberOne);
+        }
+        else if (numberTwo > numberOne) {
+            System.out.print("Большее число: " + numberTwo);
+        }
+        else {
+            System.out.print("Оба числа равны: " + numberOne + "=" + numberTwo);
+        }
     }
 }
+
